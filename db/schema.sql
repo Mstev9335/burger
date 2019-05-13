dac2e38c074e78647eeac4582708a68a644f78ca
unknown <mstevens9335@yahoo.com>
2019-05-13 17:51:43 -0400

    working logic

--- db/schema.sql
+++ db/schema.sql
@@ -8,5 +8,6 @@ CREATE TABLE burgers
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
+	createdAt TIMESTAMP DEFAULT current_timestamp NOT NULL,
   	PRIMARY KEY(id)
 );
