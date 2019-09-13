38eaebe661c0d2442ff350e0463e6399a7fd343b
Matthew Stevens <mstevens9335@yahoo.com>
2019-09-13 12:07:15 -0400

    cleaned up code

--- models/burger.js
+++ models/burger.js
@@ -2,7 +2,7 @@
 const orm = require('../config/orm.js');
 
 
-// create the code that will call the ORM functions using burger specific info for the ORM.
+// create the code that will call the ORM functions using burger specific input for the ORM.
 var burger = {
 
   selectAll: function (callback) {
